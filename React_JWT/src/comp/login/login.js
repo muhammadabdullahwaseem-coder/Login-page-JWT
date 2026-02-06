6dadad04be9865dbece8b5dabac5b06ad927d4d4
Muhammad Abdullah <muhammadabdullah121254@gmail.com>
2026-02-07 02:19:45 +0500

    .

--- React_JWT/src/comp/login/login.js
+++ React_JWT/src/comp/login/login.js
@@ -13,7 +13,7 @@ function Login() {
 
     try {
       // 👇 CONNECTS TO BACKEND LOGIN
-      const response = await fetch('https://login-page-jwt.onrender.com', {
+      const response = await fetch('https://login-page-jwt.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- React_JWT/src/comp/signup/signup.js
+++ React_JWT/src/comp/signup/signup.js
@@ -13,7 +13,7 @@ function Signup() {
 
     try {
      
-      const response = await fetch('https://login-page-jwt.onrender.com', {
+      const response = await fetch('https://login-page-jwt.onrender.com/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
