a2347b29ffa218d8dcda6727515dc5026c413d3b
Muhammad Abdullah <muhammadabdullah121254@gmail.com>
2026-03-04 20:11:34 +0500

    .

--- Node_JWT/src/routes/forgotPassword.js
+++ Node_JWT/src/routes/forgotPassword.js
@@ -32,8 +32,8 @@ router.post("/forgot-password", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: emailUser,
         pass: emailPass,
